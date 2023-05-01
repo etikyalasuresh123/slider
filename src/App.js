19450a09ee325e10c122d192052741a3c3bf3d30
etikyalasuresh123 <sureshetikyala123@gmail.com>
2023-05-01 21:13:38 +0530

    images

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import images from "./images";
+import ImageSlider from "./ImageSlider";
+  
+const App = () => {
+  return (
+    <div className="App">
+    <ImageSlider images={images}/>
+  </div>
+  )
+}
+  
+export default App;
